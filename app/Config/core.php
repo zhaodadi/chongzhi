0ee265730b526b4450239c75ab00a084e0b4c509
Dadi <zhaodadi@gmail.com>
2013-05-29 14:30:51 +1000

    salt and debug kit

--- app/Config/core.php
+++ app/Config/core.php
@@ -194,12 +194,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'zdd13608680655crytear316');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '19900316136086806550432383471');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
